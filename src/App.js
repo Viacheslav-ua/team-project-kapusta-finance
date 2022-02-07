1d8d23279397f89b363d2afe5f6cc33d287ce214
Oleksandr Okhrimovich <ohrimovich.sanek@gmail.com>
2022-02-07 15:40:56 +0200

    Add main-page markup

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import "./App.css";
 
 import AuthForm from "./components/AuthForm/AuthForm";
 import AuthPage from "./pages/AuthPage/AuthPage";
+import MainPage from "./pages/MainPage";
 
 function App() {
   return (
     <div className="App">
-      <AuthPage />
+      {/* <AuthPage /> */}
+      <MainPage/>
     </div>
   );
 }
