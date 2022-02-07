1393cdf5558d6a6d5b611cc0df2f4e7adbde31b4
AndrewRiabets <Inferion5@gmail.com>
2022-02-07 22:11:27 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -5,25 +5,24 @@ import Header from "../src/components/Header/Header";
 import AuthForm from "./components/AuthForm/AuthForm";
 import AuthPage from "./pages/AuthPage/AuthPage";
 import ExpensesPage from "./pages/ExpensesPage/ExpensesPage";
-import IncomePage from './pages/IncomePage/IncomePage';
-import Container from "./components/Container"
+import IncomePage from "./pages/IncomePage/IncomePage";
+import Container from "./components/Container";
 import AnimatedCabbage from "./components/AnimatedCabbage/AnimatedCabbage";
-
 
 function App() {
   return (
     <div className="App">
-
-//       <Routes>
-//         <Route path="/" element={<AuthPage />} />
-//         <Route path="/expenses" element={<ExpensesPage />} />
-//         <Route path="/income" element={<IncomePage />} />
-//       </Routes>
-     
+      //{" "}
+      <Routes>
+        // <Route path="/" element={<AuthPage />} />
+        // <Route path="/expenses" element={<ExpensesPage />} />
+        // <Route path="/income" element={<IncomePage />} />
+        //{" "}
+      </Routes>
       <Container>
         <Header />
         <AuthPage />
-        <AnimatedCabbage/>
+        <AnimatedCabbage />
       </Container>
     </div>
   );
