162c7e1d87efeb8a934c32a67fb7cba4cedf897a
Vladymyr Nazarenko <vpnazarenko@gmail.com>
2022-02-12 18:11:25 +0200

    main-title-markup

--- src/components/AuthPageTitle/AuthPageTitle.js
+++ src/components/AuthPageTitle/AuthPageTitle.js
@@ -1,10 +1,47 @@
 import style from "./AuthPageTitle.module.css";
+import kapustaTablet from "../../Images/Union_tablet.png";
+import kapustaMobile from "../../Images/Union_mobile.png";
+import useWindowDimensions from "../../hooks/useWindowDimensions";
 
-const AuthPageTitle = () => (
-  <div className={style.titleContainer}>
-    <h1 className={style.mainTitle}>Kapu$ta</h1>
-    <h2 className={style.title}>Smart Finance</h2>
-  </div>
-);
+const AuthPageTitle = () => {
+  const viewPort = useWindowDimensions();
+
+  return (
+    <div className={style.titleComponent}>
+      {viewPort.width >= 220 && viewPort.width < 480 && (
+        <>
+          <div className={style.titleContainer}>
+            <img
+              src={kapustaMobile}
+              className={style.kapusta_title}
+              alt="kapustaMobile"
+            />
+            <h2 className={style.title}>Smart Finance</h2>
+          </div>
+        </>
+      )}
+      {viewPort.width >= 480 && viewPort.width < 768 && (
+        <>
+          <div className={style.titleContainer}>
+            <img
+              src={kapustaTablet}
+              className={style.kapusta_title}
+              alt="kapustaMobile"
+            />
+            <h2 className={style.title}>Smart Finance</h2>
+          </div>
+        </>
+      )}
+      {viewPort.width >= 768 && (
+        <>
+          <div className={style.titleContainer}>
+            <h1 className={style.mainTitle}>Kapu$ta</h1>
+            <h2 className={style.title}>Smart Finance</h2>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
 
 export default AuthPageTitle;
