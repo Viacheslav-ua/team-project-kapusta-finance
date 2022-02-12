cb6a897dc9b17410a9ffb73b38ca8d08fe53b372
RitaDyvnych <m.dyvnych@gmail.com>
2022-02-12 19:52:52 +0200

    fix link

--- src/components/BalancePageComponents/GoToReport/GoToReports.jsx
+++ src/components/BalancePageComponents/GoToReport/GoToReports.jsx
@@ -1,17 +1,17 @@
 import sprite from '../../../Images/sprite.svg';
 import s from './GoToReports.module.css';
+import { Link } from 'react-router-dom';
 
 export default function GoToReports() {
     return (
-        <a
+        <Link
             className={s.button}
-            type="button"
-            href='/report'
+            to='/report'
         >
             <span className={s.text}>Перейти к отчетам</span>
                 <svg width="20" height="20" className={s.icon}>
                     <use href={`${sprite}#diagram`}></use>
                 </svg>
-        </a>
+        </Link>
     );
 }
