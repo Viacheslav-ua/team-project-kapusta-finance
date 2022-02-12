6c234d7e3fa52290a4dbae60f92806cc2d2be0b7
RitaDyvnych <m.dyvnych@gmail.com>
2022-02-12 21:00:00 +0200

    deleted coments

--- src/components/BalancePageComponents/TableBalanceMob/TableBalanceMob.js
+++ src/components/BalancePageComponents/TableBalanceMob/TableBalanceMob.js
@@ -23,19 +23,13 @@ const TableBalanceMob = ({type}) => {
                   </div>
                 </div>
                 <div className={s.tablesumWrapper}>
-                  <p className={`${s.tdSum} ${type !== 'income' && s.tdSumExpense}`}
-                    // style={
-                    //   type === 'income'
-                    //     ? { color: '#407946' }
-                    //     : { color: '#E7192E' }
-                    // }
-                  >
+                  <p className={`${s.tdSum} ${type !== 'income' && s.tdSumExpense}`}>
                     {type === 'income'
                       ? `${el.sum}.00 грн.`
                       : `- ${el.sum}.00 грн.`}
                   </p>
                 </div>
-                {/* <div className={s.buttonWrapper}> */}
+
                     <button className={s.deleteBtn}>
                       <svg className={s.icon}>
                         <use
@@ -44,38 +38,11 @@ const TableBalanceMob = ({type}) => {
                         />
                       </svg>
                     </button>
-                {/* </div> */}
+
               </li>
             </div>
           ))}
         </ul>
-          {/* <table className={s.tableMain}>
-            <tbody className={s.tableBody}>
-              {items.map((el) => (
-                <tr className={s.tableRow} key={el.id}>
-                  <div>
-                    <td className={s.tdDate}>{el.descr}{el.date}</td>
-                    <td className={s.tdCtg}>{el.category}</td>
-                  </div>
-                  <td className={`${s.tdSum} ${type !== 'income' && s.tdSumExpense}`}>
-                    {type === 'income'
-                      ? `${el.sum.toLocaleString('ru')}.00 грн.`
-                      : `-${el.sum.toLocaleString('ru')}.00 грн.`}
-                  </td>
-                  <td className={s.tdIcon}>
-                    <button className={s.deleteBtn}>
-                      <svg className={s.icon}>
-                        <use
-                          href={sprite + "#delete"}
-                          alt="delete transaction"
-                        />
-                      </svg>
-                    </button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table> */}
       </div>
     </>
   );
