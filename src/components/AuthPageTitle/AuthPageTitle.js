190744ae5f6a88bf3b604b6d2ba9bea6a0f065f2
Alexander Dimitryuk <dimitryuk38@gmail.com>
2022-02-15 16:10:56 +0200

    1

--- src/components/AuthPageTitle/AuthPageTitle.js
+++ src/components/AuthPageTitle/AuthPageTitle.js
@@ -2,8 +2,12 @@ import style from "./AuthPageTitle.module.css";
 import kapustaTablet from "../../Images/Union_tablet.png";
 import kapustaMobile from "../../Images/Union_mobile.png";
 import useWindowDimensions from "../../hooks/useWindowDimensions";
+import { rollIn } from "react-animations";
+import styled, { keyframes } from 'styled-components';
 
 const AuthPageTitle = () => {
+  const Rollin = styled.div`animation: 2s ${keyframes`${rollIn}`} `;
+   
   const viewPort = useWindowDimensions();
 
   return (
@@ -36,8 +40,9 @@ const AuthPageTitle = () => {
         <>
           <div className={style.titleContainer}>
             <div className={style.titleItems}>
-              <h1 className={style.mainTitle}>Kapu$ta</h1>
-              <h2 className={style.title}>Smart Finance</h2>
+              <Rollin><h1 className={style.mainTitle}>Kapu$ta</h1></Rollin>
+              <Rollin><h2 className={style.title}>Smart Finance</h2></Rollin>
+              
             </div>
           </div>
         </>
--- src/pages/AuthPage/AuthPage.js
+++ src/pages/AuthPage/AuthPage.js
@@ -1,16 +1,25 @@
 import AuthPageTitle from "../../components/AuthPageTitle";
 import AuthForm from "../../components/AuthForm";
 import Container from "../../components/Container/ContainerAuth";
+import {  slideInRight} from "react-animations";
+import styled, { keyframes } from 'styled-components';
 
 import style from "./AuthPage.module.css";
 
 const AuthPage = () => {
+  const SlideInRight = styled.div`animation: 2s ${keyframes`${slideInRight}`} `;
   return (
     <>
       <Container>
         <div className={style.authContainer}>
           <AuthPageTitle />
-          <AuthForm />
+        
+          <SlideInRight>
+            <AuthForm />
+</SlideInRight>
+            
+    
+          
         </div>
       </Container>
     </>
