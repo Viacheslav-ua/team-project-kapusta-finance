45bf7c8609931b5b1f9d18c23d38996c4489ecd9
AndrewRiabets <Inferion5@gmail.com>
2022-02-16 13:53:23 +0200

    fix

--- src/components/Reports/ReportsBalance/TotalExpensesIncome.js
+++ src/components/Reports/ReportsBalance/TotalExpensesIncome.js
@@ -1,56 +1,50 @@
-
-import { getSummary, getDate } from '../../../redux/report/report-selectors';
-import { useSelector } from 'react-redux';
-import { useEffect, useState } from 'react';
-import styles from './TotalExpensesIncome.module.css';
+import { getSummary, getDate } from "../../../redux/report/report-selectors";
+import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import styles from "./TotalExpensesIncome.module.css";
 
 function TotalExpensesIncome() {
-    const summary = useSelector(getSummary)
-    const date = useSelector(getDate)
-    const [income, SetIncome] = useState(null)
-    const [expences, SetExpences] = useState(null)
+  const summary = useSelector(getSummary);
+  const date = useSelector(getDate);
+  const [income, SetIncome] = useState(null);
+  const [expences, SetExpences] = useState(null);
 
-    console.log(date);
-    const getProfit = () => {
-        const { profit: { totalAmount } } = summary.find((el) => el.startDate === date ? date : '2022-01-01')
-        SetIncome(totalAmount)
-    }
+  console.log(date);
+  const getProfit = () => {
+    const {
+      profit: { totalAmount },
+    } = summary.find((el) => (el.startDate === date ? date : "2022-01-01"));
+    SetIncome(totalAmount);
+  };
 
-     const getCosts = () => {
-         const { costs: { totalAmount } } = summary.find((el) => el.startDate === date ? date : '2022-01-01')
-        SetExpences(totalAmount)
-     }
+  const getCosts = () => {
+    const {
+      costs: { totalAmount },
+    } = summary.find((el) => (el.startDate === date ? date : "2022-01-01"));
+    SetExpences(totalAmount);
+  };
 
-    useEffect(() => {
-        getProfit()
-        getCosts()
-    }, [date])
+  useEffect(() => {
+    getProfit();
+    getCosts();
+  }, [date]);
 
-
-    return (
-        <div className={styles.componentBox}>
-            <ul className={styles.balanceList}>
-                <li className={styles.balanceItem}>
-                    <p className={styles.balanceText}>
-                        Расходы:
-                    </p>
-                    <span className={styles.expensesValue}>
-
-                        {`- ${expences} грн.`}
-
-                    </span>
-                </li>
-                <li className={styles.balanceItem}>
-                    <p className={styles.balanceText}>
-                        Доходы:
-                    </p>
-                    <span className={styles.incomeValue}>
-                         {`+ ${profit.totalAmount} грн.`}
-                    </span>
-                </li>
-            </ul>
-      </div>
-  )  
+  return (
+    <div className={styles.componentBox}>
+      <ul className={styles.balanceList}>
+        <li className={styles.balanceItem}>
+          <p className={styles.balanceText}>Расходы:</p>
+          <span className={styles.expensesValue}>{`- ${expences} грн.`}</span>
+        </li>
+        <li className={styles.balanceItem}>
+          <p className={styles.balanceText}>Доходы:</p>
+          <span className={styles.incomeValue}>
+            {`+ ${income.totalAmount} грн.`}
+          </span>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default TotalExpensesIncome;
--- src/pages/ReportPage/ReportPage.js
+++ src/pages/ReportPage/ReportPage.js
@@ -4,60 +4,54 @@ import { NavLink } from "react-router-dom";
 import ReportsBalance from "../../components/Reports/ReportsBalance";
 import MultipleSlider from "../../components/Reports/MultipleSlider/MultipleSlider";
 import CurrentPeriod from "../../components/Reports/CurrentPeriod/CurrentPeriod";
-import useWindowDimensions from '../../hooks/useWindowDimensions';
+import useWindowDimensions from "../../hooks/useWindowDimensions";
 import Balance from "../../components/BalancePageComponents/Balance/Balance";
 import ConfirmButton from "../../components/BalancePageComponents/ConfirmButton/ConfirmButton";
-import { getSummary, getDate } from '../../redux/report/report-selectors';
+import { getSummary, getDate } from "../../redux/report/report-selectors";
 import * as actions from "../../redux/report/report-actions";
 import style from "./ReportPage.module.css";
 import s from "./bg.module.css";
 import sprite from "../../Images/sprite.svg";
 
-
-
-
-
 const ReportPage = () => {
-
-  const accessToken = useSelector(getAccessToken);
+  // const accessToken = useSelector(getAccessToken);
   const viewPort = useWindowDimensions();
   const dispatch = useDispatch();
-  const [page, setPage] = useState('report')
-    const summary = useSelector(getSummary);
+  const [page, setPage] = useState("report");
+  const summary = useSelector(getSummary);
   const reversedSummary = [...summary].reverse();
-  const currentDate = reversedSummary[0].startDate
+  const currentDate = reversedSummary[0].startDate;
 
   console.log(currentDate);
 
   useEffect(() => {
     dispatch(actions.date(currentDate));
-  },[])
- 
+  }, []);
+
   return (
-
     <div className={s.bg_contaiter}>
-
-    <div className={style.topWrapper}>
-          <NavLink to="/balance" className={style.report}>
-            <svg className={style.icon}>
-              <use href={sprite + "#arrow"} alt="Go back" />
-            </svg>
-            <p className={style.linkText}>{viewPort.width > 767 && 'Вернуться на главную'}</p>
-          </NavLink>
-          <div className={style.mobWrapper}>
+      <div className={style.topWrapper}>
+        <NavLink to="/balance" className={style.report}>
+          <svg className={style.icon}>
+            <use href={sprite + "#arrow"} alt="Go back" />
+          </svg>
+          <p className={style.linkText}>
+            {viewPort.width > 767 && "Вернуться на главную"}
+          </p>
+        </NavLink>
+        <div className={style.mobWrapper}>
           <div className={style.balance}>
-            <Balance page={page}/>
+            <Balance page={page} />
           </div>
           <CurrentPeriod />
-          </div>
-          
-    </div>
+        </div>
+      </div>
 
       <ReportsBalance />
       <div className={style.bottomWrapper}>
         <MultipleSlider />
       </div>
-      </div>
+    </div>
   );
 };
 
