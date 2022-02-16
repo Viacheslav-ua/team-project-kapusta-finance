e725e1311b07e2bc7950431aab3aa2fb950a16a9
AndrewRiabets <Inferion5@gmail.com>
2022-02-16 16:12:32 +0200

    fix-bugs

--- src/components/Reports/ReportsBalance/TotalExpensesIncome.js
+++ src/components/Reports/ReportsBalance/TotalExpensesIncome.js
@@ -1,54 +1,46 @@
-import { useSelector} from "react-redux";
-import { getSummary, getDate } from '../../../redux/report/report-selectors';
-import { useEffect, useState } from 'react';
-import styles from './TotalExpensesIncome.module.css';
+import { useSelector } from "react-redux";
+import { getSummary, getDate } from "../../../redux/report/report-selectors";
+import { useEffect, useState } from "react";
+import styles from "./TotalExpensesIncome.module.css";
 
+function TotalExpensesIncome() {
+  const summary = useSelector(getSummary);
+  const [income, setIncome] = useState(null);
+  const [expences, setExpences] = useState(null);
+  const date = useSelector(getDate);
 
-function  TotalExpensesIncome () {
-    const summary = useSelector(getSummary)
-    const [income, setIncome] = useState(null)
-    const [expences, setExpences] = useState(null)
-    const date = useSelector(getDate)
+  const getProfit = () => {
+    const {
+      profit: { totalAmount },
+    } = summary.find((el) => el.startDate === "2022-01-01");
+    setIncome(totalAmount);
+  };
 
+  const getCosts = () => {
+    const {
+      costs: { totalAmount },
+    } = summary.find((el) => el.startDate === "2022-01-01");
+    setExpences(totalAmount);
+  };
+  useEffect(() => {
+    getProfit();
+    getCosts();
+  }, [date, summary]);
 
-        const getProfit = () => {
-        const { profit: { totalAmount } } = summary.find((el) => el.startDate ===  '2022-01-01')
-        setIncome(totalAmount)
-    }
-
-         const getCosts = () => {
-         const { costs: { totalAmount } } = summary.find((el) => el.startDate === '2022-01-01' )
-        setExpences(totalAmount)
-    }
-    useEffect(() => {
-        getProfit()
-        getCosts()
-    }, [date, summary])
-
-    return (
-        <div className={styles.componentBox}>
-            <ul className={styles.balanceList}>
-                <li className={styles.balanceItem}>
-                    <p className={styles.balanceText}>
-                        Расходы:
-                    </p>
-                    <span className={styles.expensesValue}>
-
-                        {`- ${expences} грн.`}
-
-                    </span>
-                </li>
-                <li className={styles.balanceItem}>
-                    <p className={styles.balanceText}>
-                        Доходы:
-                    </p>
-                    <span className={styles.incomeValue}>
-                         {`+ ${income} грн.`}
-                    </span>
-                </li>
-            </ul>
-      </div>
-  )  
-
+  return (
+    <div className={styles.componentBox}>
+      <ul className={styles.balanceList}>
+        <li className={styles.balanceItem}>
+          <p className={styles.balanceText}>Расходы:</p>
+          <span className={styles.expensesValue}>{`- ${expences} грн.`}</span>
+        </li>
+        <li className={styles.balanceItem}>
+          <p className={styles.balanceText}>Доходы:</p>
+          <span className={styles.incomeValue}>{`+ ${income} грн.`}</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
 
 export default TotalExpensesIncome;
